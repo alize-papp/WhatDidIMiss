b45a21f19cfe08f9a4adcfac27fd9fd0a8c941f1
hapax <alize.papp@outlook.com>
2021-04-12 13:25:01 +0200

    Clean code

---
+++ src/manipulate_data.py
@@ -0,0 +1,17 @@
+import re
+
+def parse_date_in_title(title: str):
+    date_pattern = "\(\d\d\d\d\)$"
+    match = re.search(date_pattern, title)
+    if match is None:
+        return match
+    
+    date = match.group()[1:5]
+    return date
+
+def get_value_for_key(df, key, col_key, col_value):
+    found_key = df.loc[df[col_key] == key, col_value].values
+    if found_key.shape[0] != 1:
+        return None
+    
+    return found_key[0]
